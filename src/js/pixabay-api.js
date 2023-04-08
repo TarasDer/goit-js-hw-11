97dc4b698931b91713aa2f351a78e2a79388bdbb
TarasDer <tderev.switch@gmail.com>
2023-04-08 03:16:11 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,14 +2,14 @@ import axios from 'axios';
 
 export class PixabayAPI {
   #API_KEY = '35107555-f3c90346c08cd3bb4853f7523';
-  #BASE_URL = 'https://pixabay.com';
+  #BASE_URL = 'https://pixabay.com/api';
 
   page = 1;
   per_page = 40;
   q = null;
 
   fetchPhoto() {
-    return axios.get(`${this.#BASE_URL}/api?`, {
+    return axios.get(`${this.#BASE_URL}?`, {
       params: {
         key: this.#API_KEY,
         image_type: 'photo',
